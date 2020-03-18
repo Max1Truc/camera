d7dabf6f9264b546d22f58645b5ad2f38c76f403
Max1Truc <maxitruc.bonbec@gmail.com>
2020-03-18 21:11:08 +0100

    Disable audio

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var video = document.querySelector('video');
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
   .then(function(stream) {
         video.srcObject = stream;
        video.onloadedmetadata = function(e) {
